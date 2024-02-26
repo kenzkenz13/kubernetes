3a109dc2a67bc75d9ec60ac8d2558ce1d9a49930
kenzkenz13 <139207162+kenzkenz13@users.noreply.github.com>
2024-02-26 15:37:32 +0700

    setup-hosts.sh

--- vagrant-kubeadm/ubuntu/setup-hosts.sh
+++ vagrant-kubeadm/ubuntu/setup-hosts.sh
@@ -5,12 +5,11 @@ ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | c
 sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
 
 # remove ubuntu-bionic entry
-sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+sed -e '/^.*ubuntu-generic.*/d' -i /etc/hosts
 
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
-192.168.56.2 master01
-192.168.56.3 worker01
-192.168.56.4 worker02
-192.168.56.5 worker03
+172.16.16.201 master01
+172.16.16.202 worker01
+172.16.16.203 worker02
 EOF
